20d7c404805aaee0e360ac6c409a82f50a8291f1
Tina <tinaytini@yahoo.com>
2020-04-17 19:40:07 +0800

    fist commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        arrows: false
+    });
+  });
